eda3b029686f02e525f0b769cda4e1ced73af8e4
yelinxi <2790673868@qq.com>
2021-03-02 22:11:12 +0800

    init

---
+++ src/main/java/com/example/demo/ArsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArsApplication.class, args);
+    }
+
+}
