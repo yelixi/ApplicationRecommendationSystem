fccc9b88927e9407cb034bc92003d6cb67711dac
whz11 <1195687131@qq.com>
2021-03-10 23:22:58 +0800

    properties

---
+++ src/main/java/com/example/demo/conf/SchedulerThreadPoolConfig.java
@@ -0,0 +1,56 @@
+package com.example.demo.conf;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @author whz
+ * @date 2021/3/10 下午10:27
+ **/
+@Configuration
+public class SchedulerThreadPoolConfig implements AsyncConfigurer {
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        // 定时任务执行线程池核心线程数
+        taskScheduler.setPoolSize(10);
+        //设置任务注册器的调度器
+        taskScheduler.setRemoveOnCancelPolicy(true);
+        //设置线程名称前缀
+        taskScheduler.setThreadNamePrefix("SchedulerThreadPool-");
+        return taskScheduler;
+    }
+
+    @Override
+    @Bean
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor threadPool = new ThreadPoolTaskExecutor();
+        //设置核心线程数
+        threadPool.setCorePoolSize(10);
+        //设置最大线程数
+        threadPool.setMaxPoolSize(100);
+        //线程池所使用的缓冲队列
+        threadPool.setQueueCapacity(10);
+        //等待任务在关机时完成--表明等待所有线程执行完
+        threadPool.setWaitForTasksToCompleteOnShutdown(true);
+        // 等待时间 （默认为0，此时立即停止），并没等待xx秒后强制停止
+        threadPool.setAwaitTerminationSeconds(60);
+        //  线程名称前缀
+        threadPool.setThreadNamePrefix("Async-");
+        // 初始化线程
+        threadPool.initialize();
+        return threadPool;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return null;
+    }
+}
