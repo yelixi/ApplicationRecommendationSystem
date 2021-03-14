5d3ed7a85ca8fbb8b142003da5c8750f25ec0c4b
Yang Yunhan <2455219437@qq.com>
2021-03-14 10:26:27 +0800

    更新了修改密码的属性

---
+++ src/main/java/com/example/demo/param/ChangePasswordParam.java
@@ -0,0 +1,23 @@
+package com.example.demo.param;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+/**
+ * Created by yyh
+ * Date 2021/3/13 22:08
+ */
+
+@Data
+public class ChangePasswordParam {
+
+    @NotNull(message = "旧密码不能为空")
+    private String oldPassword;
+
+    @NotNull(message = "新密码不能为空")
+    private String newPassword;
+
+    @NotNull(message = "请再次确认新密码")
+    private String comfirmPassword;
+
+}
