bac87e27bb1d77eab3f10d5834fbb84bde80c479
whz11 <1195687131@qq.com>
2021-03-16 10:55:38 +0800

    头像上传

--- src/main/java/com/example/demo/controller/UserInfoController.java
+++ src/main/java/com/example/demo/controller/UserInfoController.java
@@ -1,16 +1,28 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.UserInfo;
+import com.example.demo.enums.ResultEnum;
 import com.example.demo.model.RestResult;
+import com.example.demo.model.UserInformation;
+import com.example.demo.service.ImageService;
 import com.example.demo.service.UserInfoService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.annotation.Resource;
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Objects;
 
 /**
  * (UserInfo)表控制层
  *
- * @author makejava
+ * @author whz
  * @since 2021-03-08 23:42:46
  */
 @RestController
@@ -21,16 +33,21 @@ public class UserInfoController {
      */
     @Resource
     private UserInfoService userInfoService;
-
+    @Resource
+    private ImageService imageService;
     /**
-     * 通过主键查询单条数据
-     *
-     * @param id 主键
-     * @return 单条数据
+     * 允许的图片路径
      */
-    @GetMapping("selectOne")
-    public UserInfo selectOne(Integer id) {
-        return this.userInfoService.queryById(id);
+    private static final String[] SUFFIX_WHITE_LIST = {"jpg", "png", "gif", "bmp"};
+    private static final String[] DIR_WHITE_LIST = {"avatar", "banner"};
+    private static final HashSet<String> SUFFIX_LIST;
+    private static final HashSet<String> DIR_LIST;
+
+    static {
+        SUFFIX_LIST = new HashSet<>(4);
+        DIR_LIST = new HashSet<>(2);
+        SUFFIX_LIST.addAll(Arrays.asList(SUFFIX_WHITE_LIST));
+        DIR_LIST.addAll(Arrays.asList(DIR_WHITE_LIST));
     }
 
     @PostMapping("/update")
@@ -39,4 +56,35 @@ public class UserInfoController {
         return RestResult.success(this.userInfoService.update(userupdateInfo));
     }
 
+    @PostMapping("uploadAvatar")
+    public RestResult uploadAvatar(MultipartHttpServletRequest request, String type, Principal principal) {
+        MultipartFile file = request.getFile("file");
+        if (file == null) {
+            return RestResult.error("file不存在");
+        }
+        if (!DIR_LIST.contains(type)) {
+            return RestResult.error("非法路径");
+        }
+
+        String fileName = file.getOriginalFilename();
+        Objects.requireNonNull(fileName);
+        String suffix = fileName.substring(fileName.lastIndexOf(".")).replace(".", "");
+        if (!SUFFIX_LIST.contains(suffix.toLowerCase())) {
+            return RestResult.error(ResultEnum.UNSUPPORTED_MEDIA_TYPE);
+        }
+        String url = imageService.saveFileToLocalServeWithDir(file, type);
+        if (url == null) {
+            return RestResult.error("图片 " + file.getOriginalFilename() + " 上传失败");
+        } else {
+            UserInformation userInformation = (UserInformation) principal;
+            if (userInformation == null) {
+                return RestResult.error(ResultEnum.NO_PERMISSION_ACCESS);
+            }
+            UserInfo userInfo = new UserInfo();
+            userInfo.setUserId(userInformation.getId());
+            userInfo.setAvatar(url);
+            userInfoService.update(userInfo);
+            return RestResult.success(url);
+        }
+    }
 }
---
+++ src/main/java/com/example/demo/service/ImageService.java
@@ -0,0 +1,59 @@
+package com.example.demo.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * @author whz
+ * @date 2021/3/16 上午10:26
+ **/
+@Slf4j
+@Service
+public class ImageService {
+    @Value("${media.dir}")
+    String mediaDir;
+
+    @Value("${spring.mvc.static-path-pattern}")
+    String requestPath;
+
+    @Deprecated
+    public String saveFileToLocalServe(MultipartFile srcFile) {
+        return saveFileToLocalServeWithDir(srcFile, null);
+    }
+
+    public String saveFileToLocalServeWithDir(MultipartFile srcFile, String dirname) {
+        if (srcFile.isEmpty()) {
+            return null;
+        }
+        if (dirname == null) {
+            dirname = "";
+        } else if (!dirname.endsWith("/")) {
+            dirname = dirname + "/";
+        }
+        String fileName = srcFile.getOriginalFilename();
+        assert fileName != null;
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        String newNameWithoutDir = UUID.randomUUID().toString().replace("-", "") + suffix;
+        File dest = new File(new File(mediaDir + dirname).getAbsolutePath() + "/" + newNameWithoutDir);
+        if (!dest.getParentFile().exists()) {
+            if (!dest.getParentFile().mkdirs()) {
+                log.error("写入文件时创建父目录异常，请检查权限");
+                return null;
+            }
+        }
+        try {
+            srcFile.transferTo(dest);
+        } catch (IOException e) {
+            log.error("文件转存失败");
+            e.printStackTrace();
+            return null;
+        }
+        return requestPath.replace("**", "") + dirname + newNameWithoutDir;
+    }
+}
