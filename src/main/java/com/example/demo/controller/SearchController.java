505f2bc5160ccdbf39b0c1852b25ee44032463e7
Yang Yunhan <2455219437@qq.com>
2021-04-02 22:44:40 +0800

    添加了智能填报的学校属性的相关接口的部分属性

--- src/main/java/com/example/demo/controller/SearchController.java
+++ src/main/java/com/example/demo/controller/SearchController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.SearchCondition;
+import com.example.demo.model.RestResult;
+import com.example.demo.service.ConditionService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
 *搜索志愿控制层
@@ -13,5 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/searchCondition")
 public class SearchController {
+    /**
+    *搜索对象
+     */
+    @Resource
+    private ConditionService conditionService;
+    /**
+     * 通过主键查询id
+     */
+    @GetMapping("/selectOne")
+    public SearchCondition selectOne(Integer id){
+        return this.conditionService.selectById(id);
+    }
+    /**
+     *
+     */
 
 }
