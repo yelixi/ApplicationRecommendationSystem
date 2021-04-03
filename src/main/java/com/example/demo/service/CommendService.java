cc4be68345d3df9b029da48aa65d0a7df462b948
wanglingyi <1152936469@qq.com>
2021-04-03 16:56:58 +0800

    筛选功能

--- src/main/java/com/example/demo/service/CommendService.java
+++ src/main/java/com/example/demo/service/CommendService.java
@@ -2,8 +2,11 @@ package com.example.demo.service;
 
 import com.example.demo.entity.CommendCondition;
 import com.example.demo.entity.SchoolResult;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.util.List;
 
 public interface CommendService {
 
-    SchoolResult selectByCondition(CommendCondition condition);
+    List<SchoolResult> selectByCondition(CommendCondition condition) throws JsonProcessingException;
 }
